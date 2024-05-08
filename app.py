abbe8cb146f1ba55d0c11125994f32b6fba727f3
ctripp <cullin.tripp@snowflake.com>
2024-05-08 15:05:08 -0400

    fixing envs

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ def create_snowpark_session():
         }
     else:
          connection_parameters = {
-            "account": os.getenv('SNOWFLAKE_ACCOUNT'),
-            "user": os.getenv(SNOWFLAKE_USER),
-            "password": os.getenv(SNOWFLAKE_PASSWORD),
-            "role": os.getenv(SNOWFLAKE_ROLE),
-            "warehouse": os.getenv(SNOWFLAKE_WAREHOUSE),
-            "database": os.getenv(SNOWFLAKE_DATABASE),
-            "schema": os.getenv(SNOWFLAKE_SCHEMA)
+            "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+            "user": os.getenv("SNOWFLAKE_USER"),
+            "password": os.getenv("SNOWFLAKE_PASSWORD"),
+            "role": os.getenv("SNOWFLAKE_ROLE"),
+            "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+            "database": os.getenv("SNOWFLAKE_DATABASE"),
+            "schema": os.getenv("SNOWFLAKE_SCHEMA")
         }
     session = Session.builder.configs(connection_parameters).create()
     return session
@@ -167,3 +167,6 @@ def leaderboard():
     </html>
     '''
     return html
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
